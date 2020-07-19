a78d865abf575c5e294be4abecc75644b6f3da33
Frank Truong <truongnfrank@gmail.com>
2020-07-19 17:13:03 -0400

    Hello Frank Test

---
+++ main.cpp
@@ -0,0 +1,59 @@
+// https://github.com/ThePhluffyPanda/C-Tutorial.git
+#include <cstdlib>  // import C Standard General Utilities Library
+// https://www.cplusplus.com/reference/cstdlib/
+
+#include <iostream>  // import read and write 
+
+#include <string>   // work with strings
+
+#include <limits>  // min and max values
+
+#include <vector>
+
+#include <sstream>  // string streams
+
+#include <numeric>  // sequences
+
+#include <ctime>    // time
+
+#include <cmath>    // math fucntions
+
+using namespace std;    //
+
+int main(int argc, char** argv) {
+
+    cout << "Hello Frank" << endl;
+
+    return 0;
+
+}   // end main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
