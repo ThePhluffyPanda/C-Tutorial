95607e81d5b468092b5f8c53e7d1c287abaa7f48
Frank Truong <truongnfrank@gmail.com>
2020-07-19 18:05:51 -0400

    added #include <stdio.h> allows printf to work worked with printf

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@
 
 #include <cmath>    // math fucntions
 
+#include <stdio.h>  // allows printf to work... read more here https://stackoverflow.com/questions/29886869/printf-identifier-not-found
+
 using namespace std;    //
 
 int globalVariable = 0; // Global variable
@@ -31,52 +33,43 @@ int main(int argc, char** argv) {
     bool married = false;
     char myGrade = 'A';
 
-    // There are many integer types with different min
-    // and max values
-    cout << "Min unsigned short int " <<
-            numeric_limits<unsigned short int>::min()
-            << endl;
-    cout << "Max unsigned short int " <<
-            numeric_limits<unsigned short int>::max()
-            << endl;
-    cout << "Min short int " <<
-            numeric_limits<short int>::min()
-            << endl;
-    cout << "Max short int " <<
-            numeric_limits<short int>::max()
-            << endl;
-    cout << "Min int " <<
-            numeric_limits<int>::min()
-            << endl;
-    cout << "Max int " <<
-            numeric_limits<int>::max()
-            << endl;
-    cout << "Min long " <<
-            numeric_limits<long>::min()
-            << endl;
-    cout << "Max long " <<
-            numeric_limits<long>::max()
-            << endl;
- 
-    // Floating type numbers have decimal places
-    cout << "Min float " <<
-            numeric_limits<float>::min()
-            << endl;
-    cout << "Max float " <<
-            numeric_limits<float>::max()
-            << endl;
-    cout << "Min double " <<
-            numeric_limits<double>::min()
-            << endl;
-    cout << "Max double " <<
-            numeric_limits<double>::max()
-            << endl;
-    cout << "Min long double " <<
-            numeric_limits<long double>::min()
-            << endl;
-    cout << "Max long double " <<
-            numeric_limits<long double>::max()
-            << endl;
+    float f1 = 1.1111111;
+    float f2 = 1.1111111;
+
+    printf("Sum of two floats = %.7f\n", (f1 + f2));  // lost precision on the 7th decimal place
+
+    // doubles accurate up to 15 points of precision
+    double d1 = 1.111111111111111;
+    double d2 = 1.111111111111111;
+
+    printf("Sum of two doubles = %.16f\n", (d1 + d2));  // lost precision on the 16th decimal place
+
+    //auto whatAmI = true;    // auto data type selection 
+    
+    cout << "int Byte: " << sizeof(int) << endl;    // 4 bytes in an integer
+
+    printf("%c %d %5d %.3f %s\n", 
+            'A', 10, 5, 3.1234, "Sup");
+
+    string q1 = "Enter a Number: ";
+    string num1, num2;
+
+    cout << q1;
+    cin >> num1;
+    cout << "Enter another number: ";
+    cin >> num2;
+
+    //convert strings into intergers
+    int nNum1 = stoi(num1);
+    int nNum2 = stoi(num2);
+
+    printf("%d + %d = %d", 
+            nNum1, nNum2, (nNum1 + nNum2));
+
+    
+
+
+
 
 
     return 0;
