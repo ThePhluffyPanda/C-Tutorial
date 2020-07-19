ccfd34e0752f83d231791cefd639e406cc72fa31
Frank Truong <truongnfrank@gmail.com>
2020-07-19 17:25:59 -0400

    Explored many ints, floats, and doubles

--- main.cpp
+++ main.cpp
@@ -30,7 +30,54 @@ int main(int argc, char** argv) {
 
     bool married = false;
     char myGrade = 'A';
-    
+
+    // There are many integer types with different min
+    // and max values
+    cout << "Min unsigned short int " <<
+            numeric_limits<unsigned short int>::min()
+            << endl;
+    cout << "Max unsigned short int " <<
+            numeric_limits<unsigned short int>::max()
+            << endl;
+    cout << "Min short int " <<
+            numeric_limits<short int>::min()
+            << endl;
+    cout << "Max short int " <<
+            numeric_limits<short int>::max()
+            << endl;
+    cout << "Min int " <<
+            numeric_limits<int>::min()
+            << endl;
+    cout << "Max int " <<
+            numeric_limits<int>::max()
+            << endl;
+    cout << "Min long " <<
+            numeric_limits<long>::min()
+            << endl;
+    cout << "Max long " <<
+            numeric_limits<long>::max()
+            << endl;
+ 
+    // Floating type numbers have decimal places
+    cout << "Min float " <<
+            numeric_limits<float>::min()
+            << endl;
+    cout << "Max float " <<
+            numeric_limits<float>::max()
+            << endl;
+    cout << "Min double " <<
+            numeric_limits<double>::min()
+            << endl;
+    cout << "Max double " <<
+            numeric_limits<double>::max()
+            << endl;
+    cout << "Min long double " <<
+            numeric_limits<long double>::min()
+            << endl;
+    cout << "Max long double " <<
+            numeric_limits<long double>::max()
+            << endl;
+
 
     return 0;
 
